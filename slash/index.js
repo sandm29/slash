9bc63b853eabb3614bec87ca606a514e49f60d2f
sandm29 <107444478+sandm29@users.noreply.github.com>
2023-03-21 09:38:22 -0300

    Update index.js

--- slash/index.js
+++ slash/index.js
@@ -28,3 +28,6 @@ window.onscroll = () => {
 let header = document.querySelector('header');
 
 header.classList.toggle('sticky', window.scrollY > 100);
+
+menuIcon.classList.remove('bx-x');
+navbar.classList.remove('active');
